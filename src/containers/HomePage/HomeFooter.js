41ef6520b204bb8a88897e5dcbbf1cc4040c8669
Hung Pham <hungphammanh2012003@gmail.com>
2023-01-12 14:58:06 +0700

    hoidanitclone

---
+++ src/containers/HomePage/HomeFooter.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+
+class HomeFooter extends Component {
+    render() {
+
+        return (
+            <div className='home-footer'>
+                <p>&copy; 2023 Phạm Mạnh Hùng. More information, please visit my youtube channel.
+                    <a target='_blank' href='https://www.youtube.com/watch?v=147SkAVXEqM&list=PLncHg6Kn2JT6E38Z3kit9Hnif1xC_9VqI&index=63'>
+                        &#8594; Click here &#8592;</a></p>
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeFooter);
